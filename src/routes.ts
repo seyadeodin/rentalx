8fc887b973efc445da04a631d417baf5a6552a48
lucsa <seyadeodin@gmail.com>
2022-05-25 17:46:34 -0300

    first module finished

---
+++ src/routes.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import CreateCourseService from './CreateCourseService';
+
+export function createCourse(request: Request, response: Response){
+
+  CreateCourseService.execute({
+    name:"NodeJS", 
+    educator: "Lucas",
+    duration: 10, 
+  });
+
+  return response.send();
+}
